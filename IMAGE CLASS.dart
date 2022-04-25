52082eb61c476b0fb2c8e7f9465714b316ae8058
Muhaliakbar <100175435+Muhaliakbar@users.noreply.github.com>
2022-04-25 10:01:25 -0700

    Add files via upload

---
+++ IMAGE CLASS.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DEDI AHYAR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MUH ALI AKBAR_D0219348'),
+      ),
+      body: Center(
+        child: Image(image: AssetImage('assets/profile.png')),
+      ),
+    );
+  }
+}
